dc4057cc153ab0980bc0258901efb2accdd548c1
NaoufelGreta <naoufelboutet2@gmail.com>
2025-08-01 11:36:27 +0200

    New try

--- app/__init__.py
+++ app/__init__.py
@@ -1,14 +1,11 @@
 from flask import Flask
-from app.routes.modele_routes import modele_bp
-from app.embedding_cache import load_embedding_cache 
-from app.modele_cache import load_model
+from app.routes.arango_route import arango_bp
+from app.routes.postgres_route import pg_bp
 
 def create_app():
     app = Flask(__name__)
-
-    app.register_blueprint(modele_bp)
-    load_embedding_cache()
-    load_model()
     
+    app.register_blueprint(pg_bp)
+    app.register_blueprint(arango_bp)
     
     return app
--- dockerfile
+++ dockerfile
@@ -8,4 +8,6 @@ RUN pip install --upgrade pip
 
 RUN pip install --no-cache-dir -r requirements.txt
 
+EXPOSE 5001
+
 CMD ["python", "run.py"]  
